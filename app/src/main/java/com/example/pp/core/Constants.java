269f8b6e3254230c9ce0fc73efd68d0ca4b3140a
pawan <pawanpatel12895@gmail.com>
2023-03-23 19:19:48 +0530

    Added Shared Preference Constants for storing user info in the Local App itself.

--- app/src/main/java/com/example/pp/core/Constants.java
+++ app/src/main/java/com/example/pp/core/Constants.java
@@ -2,6 +2,10 @@ package com.example.pp.core;
 
 public final class Constants {
 
+    public static final class SHARED_PREFERENCE {
+        public static final String NAME = "COLORIFY_SHARED_PREFERENCE_NAME";
+        public static final String USER_ID = "USER_ID";
+    }
     public static final class Socket {
         public static final String socketURL = "ws://192.168.195.10:8080/myHandler";
     }
