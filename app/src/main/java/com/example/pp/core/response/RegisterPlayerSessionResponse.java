4ce66b782b28a8026732faeffb6138bf067cc899
pawan <pawanpatel12895@gmail.com>
2023-03-30 17:38:00 +0530

    Added RegisterPlayerSession Request/Response

---
+++ app/src/main/java/com/example/pp/core/response/RegisterPlayerSessionResponse.java
@@ -0,0 +1,10 @@
+package com.example.pp.core.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class RegisterPlayerSessionResponse {
+    final boolean isRegistered;
+}
