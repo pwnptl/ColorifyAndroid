b45aabee9405be5bb2a1a638caaea2c1e5924b12
pawan <pawanpatel12895@gmail.com>
2023-04-13 21:40:54 +0530

    changed socketUrl

--- app/src/main/java/com/example/pp/core/Constants.java
+++ app/src/main/java/com/example/pp/core/Constants.java
@@ -7,7 +7,7 @@ public final class Constants {
         public static final String USER_ID = "USER_ID";
     }
     public static final class Socket {
-        public static final String socketURL = "ws://192.168.29.210:8080/myHandler";
+        public static final String socketURL = "ws://192.168.29.232:8080/myHandler";
         public static final long AUTOMATIC_RECONNECTION_TIMEOUT_MILLIS = 10000;
         public static final int READ_TIMEOUT_MILLIS = 60000;
         public static final int CONNECT_TIMEOUT_MILLIS = 10000;
