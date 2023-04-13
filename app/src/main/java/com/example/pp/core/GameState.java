d9a480b731995747b07359c2043b0999846fe73b
pawan <pawanpatel12895@gmail.com>
2023-04-13 21:39:38 +0530

    Added GameState Enum

---
+++ app/src/main/java/com/example/pp/core/GameState.java
@@ -0,0 +1,26 @@
+package com.example.pp.core;
+
+import lombok.Getter;
+
+public enum GameState {
+    NOT_INITIALIZED("not_initialized"),
+    INITIALIZED("initialized"),
+    WAITING_FOR_PLAYERS_TO_JOIN("waiting_for_players_to_join"),
+    ALL_PLAYER_JOINED("all_player_joined"),
+    START("ready_to_start"),
+    WAITING_FOR_PLAYER_MOVE("waiting_for_player_move"),
+    MAKE_MOVE("make_move"),
+    VALIDATE_GAME("validate_game"),
+    FINISH("finish"),
+    AWARDS("award_player"),
+    TERMINATE("terminate");
+
+
+    @Getter
+    private String value;
+
+    GameState(String value) {
+        this.value = value;
+    }
+
+}
