aeef9778aae59efa03cf9fd7d34e526f5902b4af
pawan <pawanpatel12895@gmail.com>
2023-04-25 17:16:13 +0530

    imporved PaletteView to include Ids in the buttons.

--- app/src/main/java/com/example/pp/core/customViews/PaletteView.java
+++ app/src/main/java/com/example/pp/core/customViews/PaletteView.java
@@ -2,29 +2,19 @@ package com.example.pp.core.customViews;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.pp.colorify.R;
-
-import java.util.ArrayList;
 
 public class PaletteView extends LinearLayout {
 
     private int buttonCount;
-    private final ArrayList<Integer> buttonColors = new ArrayList<Integer>() {
-        {
-            add(Color.CYAN);
-            add(Color.BLUE);
-            add(Color.YELLOW);
-            add(Color.GREEN);
-            add(Color.RED);
-            add(Color.MAGENTA);
-        }
-    };
+
     private final int marginWidth = 3;
 
     public PaletteView(Context context) {
@@ -61,16 +51,26 @@ public class PaletteView extends LinearLayout {
         LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         layoutParams.setMargins(marginWidth, marginWidth, marginWidth, marginWidth);
         for (int i = 0; i < buttonCount; ++i) {
-            Button b = getButton(context);
+            Button b = getButton(context, "custom_palette_button_" + i);
             b.setLayoutParams(layoutParams);
-            b.setBackgroundColor(buttonColors.get(i));
+//            b.setBackgroundColor(buttonColors.get(i));
             addView(b);
         }
     }
 
 
-    private Button getButton(Context context) {
+    private Button getButton(Context context, String id) {
         Button b = new Button(context);
+//        Log.i(this.getClass().getName(), "buttonId " + id);
+        int resourceId = getResources().getIdentifier(id, "id", null);
+//        Log.i(this.getClass().getName(), "buttonId " + resourceId);
+        b.setId(resourceId);
+        b.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "button clicked" + b.getId(), Toast.LENGTH_SHORT).show();
+            }
+        });
         return b;
     }
 }
