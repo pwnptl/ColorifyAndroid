89ab85992a2aaec5841971e3a1f4ddb37b1948ee
pawan <pawanpatel12895@gmail.com>
2023-04-25 17:41:45 +0530

    Added index

--- app/src/main/java/com/example/pp/core/models/ColorifyColor.java
+++ app/src/main/java/com/example/pp/core/models/ColorifyColor.java
@@ -2,6 +2,8 @@ package com.example.pp.core.models;
 
 import android.graphics.Color;
 
+import lombok.Getter;
+
 public enum ColorifyColor {
     CYAN(Color.CYAN),
     BLUE(Color.BLUE),
@@ -10,9 +12,15 @@ public enum ColorifyColor {
     RED(Color.RED),
     MAGENTA(Color.MAGENTA);
 
+    @Getter
     private final int color;
 
     ColorifyColor(int color) {
         this.color = color;
+    }
+
+    // todo: below method is only for IntegerCell type.
+    public static ColorifyColor valueOf(int cellValue) {
+        return values()[cellValue];
     }
 }
