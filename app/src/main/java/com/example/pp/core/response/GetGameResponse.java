089a230fca461f5e3dc989c1de13c63b0bfcd40c
pawan <pawanpatel12895@gmail.com>
2023-05-02 21:33:54 +0530

    Add ScoreTracker in GetGameResponse

--- app/src/main/java/com/example/pp/core/response/GetGameResponse.java
+++ app/src/main/java/com/example/pp/core/response/GetGameResponse.java
@@ -3,6 +3,7 @@ package com.example.pp.core.response;
 import com.example.pp.core.GameState;
 import com.example.pp.core.models.Board;
 import com.example.pp.core.models.ColorifyPalette;
+import com.example.pp.core.models.ScoreTracker;
 
 import java.util.ArrayList;
 
@@ -24,5 +25,7 @@ public class GetGameResponse extends Response {
 
     private final Board board;
     private final ColorifyPalette palette;
+
+    private final ScoreTracker scoreTracker;
 
 }
